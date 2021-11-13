b4a15edcf3fd214cc65a758dc8a80b1c779825c4
Stephen <stefanodinucci@gmail.com>
2021-11-13 11:14:45 +0100

    Subject

---
+++ CosmosDB/Controllers/CosmosController.cs
@@ -0,0 +1,60 @@
+﻿using CosmosDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosDB.Controllers
+{
+    [ApiController]
+    [Route("api/locations")]
+    public class CosmosController : Controller
+    {
+        CosmosClient cosmosClient;
+        Container cosmosContainer;
+
+        public CosmosController(CosmosClient client)
+        {
+            cosmosClient = client;
+            cosmosContainer = client.GetContainer("step-cosmos-db", "locations");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                location.Id = Guid.NewGuid().ToString();
+
+                var response = await cosmosContainer.CreateItemAsync(
+                    location, new PartitionKey(location.Country));
+
+                return StatusCode(((int)response.StatusCode));
+            }
+            return Problem();
+        }
+
+        [HttpGet]
+        public async Task<List<Location>> GetAll()
+        {
+            var results = new List<Location>();
+
+            //Sql way
+            var iterator = cosmosContainer.GetItemQueryIterator<Location>();
+
+            //Linq Way
+            iterator = cosmosContainer.GetItemLinqQueryable<Location>().Where(x => x.Country == "Bob").ToFeedIterator();
+
+            while (iterator.HasMoreResults)
+            {
+                results.AddRange((await iterator.ReadNextAsync()).ToList());
+            }
+
+            return results;
+        }
+
+    }
+}
---
+++ CosmosDB/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmosDB.Models
+{
+    public class Location
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        public string Name { get; set; }
+    }
+}
